d5f026085f9b5a384691dabe153bbec664c6fffb
MaximLyoen <maxim.lyoen@gmail.com>
2023-09-12 13:48:34 +0200

    fg

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 var typed = new Typed(".typing",{
-    strings:["Maxim","Lyoen","Codeur","Développeur",""],
-    typeSpeed:100,
-    BackSpeed:100,
+    strings:["Maxim Lyoen","Développeur"],
+    typeSpeed:80,
+    BackSpeed:20,
     loop:true
 })
+document.getElementById("language").innerHTML = 'Français';
+document.getElementById("language").onclick = function(){
+    button = document.getElementById("language");
+    if(button.textContent == "English"){
+        button.textContent = 'Français';
+    }else{
+        button.textContent = 'English';
+    }
+}
+
+/*def changeLanguage():
+    if document.getElementById("language").innerHTML == "English":
+        
+    else:
+   */     
