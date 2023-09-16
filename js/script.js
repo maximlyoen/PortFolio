f1f1fc4ebc93d311f72a636b09aaeca83ff2cf86
Maxim Lyoen <maxim.lyoen@interaction-healthcare.com>
2023-09-16 23:23:33 +0200

    fgg

--- js/script.js
+++ js/script.js
@@ -1,21 +1,55 @@
-var typed = new Typed(".typing",{
-    strings:["Maxim Lyoen","Développeur"],
-    typeSpeed:150,
-    BackSpeed:200,
-    loop:true
-})
-document.getElementById("language").innerHTML = 'Français';
-document.getElementById("language").onclick = function(){
-    button = document.getElementById("language");
-    if(button.textContent == "English"){
-        button.textContent = 'Français';
-    }else{
-        button.textContent = 'English';
-    }
+let typed = null;  // Déclarer la variable typed en dehors de la fonction pour éviter les redéclarations
+initializeTyped(["Maxim Lyoen", "Développeur"])
+changeLanguage();
+function initializeTyped(strings) {
+  if (typed) {
+    typed.destroy();  // Détruire l'instance précédente de Typed.js
+  }
+
+  typed = new Typed(".typing", {
+    strings: strings,
+    typeSpeed: 50,
+    backSpeed: 50,  // Notez la correction de la propriété backSpeed
+    loop: true
+  });
 }
 
-/*def changeLanguage():
-    if document.getElementById("language").innerHTML == "English":
-        
-    else:
-   */     
+document.getElementById("buttonLanguage").onclick = function(){
+    button = document.getElementById("buttonLanguageText");
+    if(button.textContent == "English"){
+        button.textContent = 'Français';
+        changeLanguage();
+    }else{
+        button.textContent = 'English';
+        changeLanguage();
+    }
+};
+
+
+function changeLanguage() {
+    const buttonLanguageText = document.getElementById("buttonLanguageText").innerHTML;
+    if (buttonLanguageText === "English") {
+        initializeTyped(["Maxim Lyoen", "Developer"]);
+    } else {
+        initializeTyped(["Maxim Lyoen", "Développeur"]);
+    }
+    if (buttonLanguageText === "English") {
+        /*--------------------NavBar--------------------*/
+        document.getElementById("BonjourJeSuis").textContent = "Hello, I'm";
+        document.querySelector(".Présentation").textContent = "Presentation";
+        document.querySelector(".Portfolio").textContent = "Portfolio";
+        document.querySelector(".Compétences").textContent = "Skills";
+        document.querySelector(".Expérience").textContent = "Experience";
+        document.querySelector(".Contact").textContent = "Contact";
+        /*--------------------MainPage--------------------*/
+    } else {
+        /*--------------------NavBar--------------------*/
+        document.getElementById("BonjourJeSuis").textContent = "Bonjour, je suis";
+        document.querySelector(".Présentation").textContent = "Présentation";
+        document.querySelector(".Portfolio").textContent = "Portfolio";
+        document.querySelector(".Compétences").textContent = "Compétences";
+        document.querySelector(".Expérience").textContent = "Expérience";
+        document.querySelector(".Contact").textContent = "Contact";
+        /*--------------------MainPage--------------------*/
+    }
+};
